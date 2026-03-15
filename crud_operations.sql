fb0d346fb767203788da3942fc692950e31a727f
Ayden Mathews <AYDENWM@email.sc.edu>
2026-03-14 19:33:58 -0700

    Add CRUD operations for Project 1

---
+++ crud_operations.sql
@@ -0,0 +1,29 @@
+USE csce548_project1;
+
+-- CREATE
+INSERT INTO customers (first_name, last_name, email, phone)
+VALUES ('Test', 'User', 'test.user@email.com', '555-9999');
+
+-- READ
+SELECT * 
+FROM customers
+WHERE email = 'test.user@email.com';
+
+-- UPDATE
+UPDATE customers
+SET phone = '555-1111'
+WHERE email = 'test.user@email.com';
+
+-- READ AGAIN to prove update worked
+SELECT * 
+FROM customers
+WHERE email = 'test.user@email.com';
+
+-- DELETE
+DELETE FROM customers
+WHERE email = 'test.user@email.com';
+
+-- READ AGAIN to prove delete worked
+SELECT * 
+FROM customers
+WHERE email = 'test.user@email.com';
